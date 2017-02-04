f058875fcd602e9e02cb3c7c35dc768bd87b447c
SeanRemedios <14sr84@queensu.ca>
2017-02-04 13:08:03 -0500

    updated code, DNW properly, fixes to be made, Read

--- Translate.py
+++ Translate.py
@@ -1,13 +1,24 @@
 import re
 from csv import reader
 
+FUNCTION_DEFS = {
+    'abs(', 'dict(', 'help(', 'min(', 'setattr(', 'all(', 'dir(', 'hex(', 'next(', 'slice(',
+    'any(', 'divmod(', 'id(', 'object(', 'sorted(', 'ascii(', 'enumerate(', 'input(', 'oct(',
+    'staticmethod(', 'bin(', 'eval(', 'int(', 'open(', 'str(', 'bool(', 'exec(', 'isinstance(',
+    'ord(', 'sum(', 'bytearray(', 'filter(', 'issubclass(', 'pow(', 'super(', 'bytes(', 'float(',
+    'iter(', 'print(', 'tuple(', 'callable(', 'format(', 'len(','property(', 'type(', 'chr(',
+    'frozenset(', 'list(', 'range(', 'vars(','classmethod(', 'getattr(', 'locals(', 'repr(', 'zip(',
+    'compile(', 'globals(', 'map(', 'reversed(', 'complex(', 'hasattr(', 'max(', 'round(', 'delattr(',
+    'hash(', 'memoryview(', 'set('
+}
+
 def split(string):
     for i in string:
         if i == ' ':
-            linespl = (re.split(r'[ ]+', string))
+            linespl = (re.split(r'[ ]+', string, 1))
             break
         elif i == '(':
-            linespl = re.split(r'[(]+', string)
+            linespl = re.split(r'[(]+', string, 1)
             break
 
     return linespl[0], linespl[1]
@@ -21,15 +32,36 @@ def restFormat(function, rest):
  #   r = re.compile(r'(?:[^,(]|\([^)]*\))+')
  #   r.findall(rest)
 
+##    findF = ''
+##    listOfFs = []
+##    countQs = 0
+##    for el in rest:
+##        if el[0] == '"' and (countQs is not 2):
+##            countQs += 1
+##            continue
+##        else:
+##            findF += el
+##        if countQs == 2:
+##            countQs = 0
+##            print(findF)
+##            findF = ''
+
+    listOfFs = []
+    for func in FUNCTION_DEFS:
+        index = rest.find(func)
+        if index >= 0:
+            listOfFs.append({'Func': func, 'Index': index}) 
+            print(func, index)
+
     restList = []
-    
     for line in reader(rest):
-        if line[0] == '1':
-            print(True)
+        if line == 
         restList.append(line)
-    
+    print(restList)
+
     lis = []
     lis.append({'Value': function, 'Type': 'function'})
+
     for i in restList:
         if i[0] == ' ' or i[0] == '' or i[0] == ')':
             continue
@@ -43,8 +75,10 @@ def restFormat(function, rest):
             lis.append({'Value': i[0], 'Type': 'ass'})
         elif type(i[0]) is str:
             lis.append({'Value': i[0], 'Type': 'string'})
+        else:
+            lis.append({'Value': i[0], 'Type': 'function'})
             
-    print(lis)
+ #   print(lis)
  #   other = (re.split(r'[,)]+', rest))
  #   formatString = 
 #    codeDict = {'Value': other[0], 'Type': 
@@ -52,10 +86,33 @@ def restFormat(function, rest):
    # print(insideQ)
 
 def main():
-    function, rest = split('print("Hello 2,World", "1", "Test")')
+    function, rest = split('print("Hello 2,World", round(3.14), "1", "T", abs(12))')
+    print(function)
+    print(rest)
     restFormat(function, rest)
 
 main()
+
+
+
+##    restList = []
+##    fullFunc = []
+##    count = 0
+##    funcCount = 0
+##    for line in reader(rest):
+##        print(len(line))
+##        if count >= int(listOfFs[funcCount]['Index']):
+##            fullFunc.append(line)
+##            count += 1
+##            continue
+##        elif count == len(listOfFs[funcCount]['Func'])+count:
+##            restList.append(fullFunc)
+##            count += 1
+##            funcCount = 0
+##            continue
+##        restList.append(line)
+##        count += 1
+##    print(restList)
 
 
 
